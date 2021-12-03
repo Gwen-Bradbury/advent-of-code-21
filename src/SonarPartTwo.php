79ad355da9e56d58aafe19b59ba9654abe7b13ab
Gwen <charmed_89@msn.com>
2021-12-03 12:48:26 +0000

    Refactoring after three passing tests for SonarPartTwo

--- src/SonarPartTwo.php
+++ src/SonarPartTwo.php
@@ -6,15 +6,19 @@ class SonarPartTwo
 {
     function SweepPartTwo(string $puzzleInput): int
     {
-        $measurement = 6;
-        if($puzzleInput === "1\n2\n3\n4"){
-           $measurement = 9;
-        } elseif ($puzzleInput === "1\n2\n3\n4\n5") {
-            $measurement = 12;
+        $puzzleInput = explode("\n", $puzzleInput);
+        $previousMeasurement = (int)$puzzleInput[0] + (int)$puzzleInput[1] + (int)$puzzleInput[2];
+        $counter = 0;
+        forEach ($puzzleInput as $measurement) {
+            if ($measurement > $previousMeasurement) {
+                $counter++;
+            }
+        $previousMeasurement = $measurement;
         }
-        return $measurement;
+        return $counter;
     }
 }
+
 
 //$puzzleInput = explode("\n", $puzzleInput);
 //$counter = 0;
@@ -26,3 +30,14 @@ class SonarPartTwo
 //    $previousMeasurement = $measurement;
 //}
 //return $counter;
+
+
+//Three passing tests
+//$measurement = 6;
+//if($puzzleInput === "1\n2\n3\n4"){
+//    $measurement = 9;
+//} elseif ($puzzleInput === "1\n2\n3\n4\n5") {
+//    $measurement = 12;
+//}
+//return $measurement;
+//}
