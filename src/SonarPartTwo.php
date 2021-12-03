1a011699f23e280ede4fbc00d78be49aca4f4087
Gwen <charmed_89@msn.com>
2021-12-03 15:30:07 +0000

    Re-written tests for PR review

--- src/SonarPartTwo.php
+++ src/SonarPartTwo.php
@@ -6,15 +6,6 @@ class SonarPartTwo
 {
     function SweepPartTwo(string $subData): int
     {
-        $subData = explode("\n", $subData);
-        $previousMeasurement = (int)$subData[0] + (int)$subData[1] + (int)$subData[2];
-        $counter = 0;
-        forEach ($subData as $measurement) {
-            if ($measurement > $previousMeasurement) {
-                $counter++;
-            }
-        $previousMeasurement = $measurement;
-        }
-        return $counter;
+        return 0;
     }
 }
--- tests/SonarPartTwoTest.php
+++ tests/SonarPartTwoTest.php
@@ -7,18 +7,8 @@ use PHPUnit\Framework\TestCase;
 
 class SonarPartTwoTest extends TestCase
 {
-    public function  testGivenOneWindowReturnsSix(): void
+    public function  testItReturnsZeroIfNoIncrease(): void
     {
-        $this->assertSame(6, (new SonarPartTwo())->SweepPartTwo("1\n2\n3"));
-    }
-
-    public function  testGivenSecondWindowReturnsNine(): void
-    {
-        $this->assertSame(9, (new SonarPartTwo())->SweepPartTwo("2\n3\n4"));
-    }
-
-    public function  testGivenThirdWindowReturns(): void
-    {
-        $this->assertSame(12, (new SonarPartTwo())->SweepPartTwo("3\n4\n5"));
+        $this->assertSame(0, (new SonarPartTwo())->SweepPartTwo("1\n2\n3"));
     }
 }
